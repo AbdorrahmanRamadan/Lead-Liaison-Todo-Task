fdbc68847ea5642f1990ecbaacd3f3e08d438a7e
AbdorrahmanRamadan <AbdorrahmanRamadan@gmail.com>
2022-06-22 05:41:32 +0200

    Created store function to handle creating a todo task

--- laravel-todo-api/app/Http/Controllers/TodoController.php
+++ laravel-todo-api/app/Http/Controllers/TodoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\TodoResource;
 use App\Models\Todo;
+use Exception;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
@@ -11,5 +12,14 @@ class TodoController extends Controller
     public function index()
     {
         return TodoResource::collection(Todo::all());
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            return response()->json(new TodoResource(Todo::create($request->all())));
+        } catch (Exception $exception) {
+            return response()->json(['exception' => $exception->getMessage()]);
+        }
     }
 }
