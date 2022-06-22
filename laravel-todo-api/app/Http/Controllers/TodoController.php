7fb668c932f1cd81126c7523676d166ef7af1fc5
AbdorrahmanRamadan <AbdorrahmanRamadan@gmail.com>
2022-06-22 05:05:52 +0200

    Created the index function and updated the resource route name for naming convention

--- laravel-todo-api/app/Http/Controllers/TodoController.php
+++ laravel-todo-api/app/Http/Controllers/TodoController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TodoResource;
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function index()
+    {
+        return TodoResource::collection(Todo::all());
+    }
 }
